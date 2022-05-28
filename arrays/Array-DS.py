6b8d8f1db1a7e59d06d658b4bd9e1bf6e2d31687
Ved Jha <vedprakash@fanclash.in>
2022-05-28 16:46:53 +0530

    reverse array implementation in python

---
+++ arrays/Array-DS.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    # Write your code here
+
+    rev_list = []
+
+    length = len(arr)
+    for ind in range(length):
+        print(arr[length-ind-1], end='\n')
+        rev_list.append(arr[length-ind-1])
+
+    return rev_list
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
+    
